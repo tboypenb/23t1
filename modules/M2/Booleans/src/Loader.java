63889dfde1a0df914d6ede651b1bab085c633384
tboypenb <valerserg66@ya.ru>
2020-01-05 03:48:16 +0300

    Homework 2.

--- modules/M2/Booleans/src/Loader.java
+++ modules/M2/Booleans/src/Loader.java
@@ -10,19 +10,19 @@ public class Loader {
     int appleCount = 8; // items
 
     //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-    if(powderAmount = 400 && sugarAmount = 10 && milkAmount = 1000 && oilAmount = 30)
+    if( powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30)
     {
       System.out.println("Pancakes");
     }
 
     //milk - 300 ml, powder - 5 g, eggs - 5
-    if(milkAmount = 300 && powderAmount = 5 && eggsCount = 5)
+    if(milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5)
     {
     System.out.println("Omelette");
     }
 
     //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-    if(appleCount = 3 && milkAmount = 100 && powderAmount = 300 && eggsCount = 4)
+    if(appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4)
     {
       System.out.println("Apple pie");
     }
